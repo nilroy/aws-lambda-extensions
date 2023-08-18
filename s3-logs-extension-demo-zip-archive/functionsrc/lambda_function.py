659e8af70cf90958d7e9c95ba63a77ca98de39f8
Nilanjan Roy <nilanjan.roy@smartly.io>
2023-08-18 14:05:44 +0300

    working with the test lambda

--- s3-logs-extension-demo-zip-archive/functionsrc/lambda_function.py
+++ s3-logs-extension-demo-zip-archive/functionsrc/lambda_function.py
@@ -8,7 +8,15 @@ from datetime import datetime
 
 
 def lambda_handler(event, context):
-    print(f"Function: Logging something which logging extension will send to S3")
-    # log = {"log": "Logging something which logging extension will send to S3"}
-    # print(log)
+    # print(f"Function: Logging something which logging extension will send to S3")
+    json_log = {
+        "log": "Logging something which logging extension will send to 'S3'",
+        "logTime": datetime.now().isoformat(),
+        "fruits": ["apple", "orange"],
+    }
+    print(json.dumps(json_log))
+    non_json_log = "This is some random text that is not json - emitted using json.dumps() method"
+    print(json.dumps(non_json_log))
+    non_json_log_2 = "This is some random text that is not json - emitted raw"
+    print(non_json_log_2)
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
